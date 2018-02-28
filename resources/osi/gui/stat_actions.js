e14150422b4fc54f84afd6fc0872792b4242f876
Chris Colehour <chris.colehour@mentice.com>
2018-02-28 16:19:53 -0600

    Update character creation web gui

---
+++ resources/osi/gui/stat_actions.js
@@ -0,0 +1,58 @@
+var statPoints = 25;
+var str = 0;
+var dex = 0;
+var int = 0;
+
+function incStr() {
+    if (statPoints > 0) {
+        statPoints--;
+        str++;
+        document.getElementById("str").innerHTML = str;
+        document.getElementById("available").innerHTML = statPoints;
+    }
+}
+
+function decStr() {
+    if (str > 0) {
+        statPoints++;
+        str--;
+        document.getElementById("str").innerHTML = str;
+        document.getElementById("available").innerHTML = statPoints;
+    }
+}
+
+function incDex() {
+    if (statPoints > 0) {
+        statPoints--;
+        dex++;
+        document.getElementById("dex").innerHTML = dex;
+        document.getElementById("available").innerHTML = statPoints;
+    }
+}
+
+function decDex() {
+    if (dex > 0) {
+        statPoints++;
+        dex--;
+        document.getElementById("dex").innerHTML = dex;
+        document.getElementById("available").innerHTML = statPoints;
+    }
+}
+
+function incInt() {
+    if (statPoints > 0) {
+        statPoints--;
+        int++;
+        document.getElementById("int").innerHTML = int;
+        document.getElementById("available").innerHTML = statPoints;
+    }
+}
+
+function decInt() {
+    if (int > 0) {
+        statPoints++;
+        int--;
+        document.getElementById("int").innerHTML = int;
+        document.getElementById("available").innerHTML = statPoints;
+    }
+}
