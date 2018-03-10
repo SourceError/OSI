7bf60783627986f65274f072a1e342d4896f66f9
SourceError <ccolehour@gmail.com>
2018-03-10 12:54:22 -0600

    s

--- resources/osi/main/server.lua
+++ resources/osi/main/server.lua
@@ -60,7 +60,7 @@ AddEventHandler('osi:server:createCharacter', function(data)
 end)
 
 AddEventHandler('osi:server:characterJoin', function(data)
-    players[source].character_id = data.id
+    osi.players[source].character_id = data.id
     local character = osi.sql.get_character_data(data.id)
     TriggerClientEvent('osi:client:characterJoined', -1, character)
 end)
