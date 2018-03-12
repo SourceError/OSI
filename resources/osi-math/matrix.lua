776e1c267129ab967a8681511a4e2a81ee079c20
SourceError <ccolehour@gmail.com>
2018-03-12 18:31:38 -0500

    s

--- resources/osi-math/matrix.lua
+++ resources/osi-math/matrix.lua
@@ -2,7 +2,7 @@ Matrix = {}
 
 function Matrix.RotationMatrixAsVecs(direction)
     local forward = Vec:Vec(direction.x, direction.y, direction.z)
-    local right = Vector:up():cross(forward):normalize()
+    local right = Vec:up():cross(forward):normalize()
     local up = forward:cross(right):normalize()
 
     return right, forward, up
