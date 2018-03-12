011f0b3af1c7cfa9fac59b6672cae5b5cebb0942
SourceError <ccolehour@gmail.com>
2018-03-11 23:32:21 -0500

    s

--- resources/osi/main/client.lua
+++ resources/osi/main/client.lua
@@ -262,7 +262,7 @@ Citizen.CreateThread(function()
     c = scaleVec(w, -mouse.y)
 
     local ray_dir = {}
-    ray_dir.x = (a.x + v_p.x + c.x)
+    ray_dir.x = -(a.x + v_p.x + c.x)
     ray_dir.y = -(a.y + v_p.y + c.y)
     ray_dir.z = (a.z + v_p.z + c.z)
 
